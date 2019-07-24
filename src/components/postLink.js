7413bf43fcc0bfc09ea3de4d1134937e7ca71031
Operel <oriperelman@gmail.com>
2019-07-24 22:49:00 +0300

    make blog post index responsive using css grid

--- src/components/postLink.js
+++ src/components/postLink.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "gatsby";
 import Img from 'gatsby-image';
+import '../assets/allStyles.css';
 
 const PostLink = ({ postData, image }) => {
   const excerpt = postData.excerpt.replace(/ *<h2>.*?<\/h2> */g, "");
@@ -8,11 +9,11 @@ const PostLink = ({ postData, image }) => {
   <div className="post-link">
     <Link to={postData.fields.slug}>
       <div className="grid">
-        <div style={{ marginRight: '2%' }}>
+        <div>
           <h3 dangerouslySetInnerHTML={{ __html: postData.frontmatter.title }}></h3>
           <p dangerouslySetInnerHTML={{ __html: excerpt }}></p>
         </div>
-        <Img fixed={image} />
+        <Img fixed={image} className="post-thumbnail" />
       </div>
     </Link>
   </div>
