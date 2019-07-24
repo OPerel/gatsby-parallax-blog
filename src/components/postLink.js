9a69b47f60eff6c429368f1897b4d87318bec587
Operel <oriperelman@gmail.com>
2019-07-25 02:49:15 +0300

    fix regex

--- src/components/postLink.js
+++ src/components/postLink.js
@@ -4,8 +4,8 @@ import Img from 'gatsby-image';
 import '../assets/allStyles.css';
 
 const PostLink = ({ postData, image }) => {
-  const heading = postData.excerpt.match(/ *<h2>.*?<\/h2> */g).join('');
-  const excerpt = postData.excerpt.replace(/ *<h2>.*?<\/h2> */g, "");
+  const heading = postData.excerpt.match(/ <h2>.*?<\/h2> /g);
+  const excerpt = postData.excerpt.replace(/ <h2>.*?<\/h2> /g, "");
   return (
   <div className="post-link">
     <p dangerouslySetInnerHTML={{ __html: postData.frontmatter.date }}></p>
