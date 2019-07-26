d246895a22548774df4e225c47056da0c9074203
Operel <oriperelman@gmail.com>
2019-07-26 15:48:25 +0300

    disable parallax inside footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,15 +3,11 @@ import { Parallax } from 'react-scroll-parallax';
 
 const Footer = () => (
   <footer>
-    <Parallax y={["30px", "-100px"]}>
-      <p>My G-Blog</p>
-    </Parallax>
-    <Parallax y={["30px", "-100px"]}>
-      <p>
-        © {new Date().getFullYear()}, Built with{" "}
-        <a href="https://www.gatsbyjs.org">Gatsby</a>
-      </p>
-    </Parallax>
+    <p>My G-Blog</p>
+    <p>
+      © {new Date().getFullYear()}, Built with{" "}
+      <a href="https://www.gatsbyjs.org">Gatsby</a>
+    </p>
   </footer>
 )
 
