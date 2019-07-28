fd9280d4d106a3ac3ab49661128b269b85aca005
Operel <oriperelman@gmail.com>
2019-07-28 16:37:55 +0300

    add gatsby-bg-img to layout bg

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,20 +1,36 @@
 import React from "react";
+import { StaticQuery, graphql } from 'gatsby';
 import Header from "./header";
 import Footer from './footer';
+import BgImage from 'gatsby-background-image';
 import "../assets/allStyles.css"
 
 const Layout = ({ children, pageTitle }) => {
   return (
-    <div style={{ position: 'relative' }}>
-      <Header pageTitle={pageTitle} />
-      <main className="bg-container">
-        {children}
-        <div style={{position: 'sticky', bottom: '0'}}>
-          <div className="tri" />
+    <StaticQuery
+      query={graphql`{
+        file(name: {eq: "bg1.gif"}) {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }`} render={data => {(
+        <div style={{ position: 'relative' }}>
+          <Header pageTitle={pageTitle} />
+          <main className="bg-container">
+            <BgImage fluid={{data.file.childImageSharp.fluid}}>
+              {children}
+            </BgImage>
+            <div style={{position: 'sticky', bottom: '0'}}>
+              <div className="tri" />
+            </div>
+          </main>
+          <Footer />
         </div>
-      </main>
-      <Footer />
-    </div>
+      )}}
+    />
   )
 }
 
