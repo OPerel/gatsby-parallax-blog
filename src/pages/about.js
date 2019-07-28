3d15e8d7dd5322eef9bd86eb30723a0f32b3ad01
Operel <oriperelman@gmail.com>
2019-07-28 12:33:14 +0300

    about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,19 +1,23 @@
 import React from "react";
-// import { Link } from "gatsby";
-// import { Parallax } from 'react-scroll-parallax';
 import Layout from "../components/layout";
-// import Footer from '../components/footer'
 import '../assets/allStyles.css'
 
-const About = ({ location }) => (
-    <Layout location={location}>
-      <div style={{ height: '100vh' }}>
-        <div className="about">
-          <h1>Hello!</h1>
-          <p>lorem ipsum what haapend to the snippet??</p>
+const About = () => (
+  <Layout>
+    <div className="about">
+      <div>
+
+        <div className="about-head">
+          <h2>Hello!</h2>
+          <p>My name is Ori</p>
         </div>
+        <div id="my-photo"></div>
+        <p>I'm a Philosopher turned Developer. After devoting the last 8 years of my life to studying subjects such as political philosophy, philosophy of moral, and philosophical and mathematical logic, I decided to try something new. What motivated this decision?</p>
+        <p>I think that nowadays everybody should have some minimal understanding of computers and programming (without neglecting their understanding of major philosophical issues of course <span role="img" aria-label="wink">😉</span>). On a personal level, after years of concentrating in abstract and unsolvable issues I wanted to do something real, to build something I can see, and can be practical. Even a simple “Hello World!” program can be a huge satisfaction.</p>
+        <p>I would love to hear from you! Please contact me with any suggestions, comments remarks or  an offer for a front-end developer position…</p>
       </div>
-    </Layout>
+    </div>
+  </Layout>
 )
 
 export default About;
