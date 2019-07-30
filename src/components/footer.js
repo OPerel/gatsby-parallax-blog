c5245c5dc3fb68a1baec50f6daffeb1e9035fd5e
Operel <oriperelman@gmail.com>
2019-07-30 16:23:52 +0300

    insert dynamic title and author to footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,25 +1,43 @@
 import React from 'react';
+import { StaticQuery, graphql } from 'gatsby';
+
+const query = graphql`{
+  site {
+    siteMetadata {
+      title
+      author
+    }
+  }
+}`
 
 const Footer = () => (
-  <footer>
-    <div className="footer-wrapper">
-      <div className="footer-info">
-        <h5>Parallax Blog</h5>
-        <span style={{ fontSize: '0.85rem' }}>© {new Date().getFullYear()}, By Ori Perelman</span>
-      </div>
-      <div className="contact">
-        <a href="mailto: oriperelman@gmail.com">
-          <i className="far fa-envelope"></i>
-          <span>Drop a line.</span>
-        </a>
-        <a href="https://github.com/OPerel" target="_blank" rel="noopener noreferrer">
-          <i className="fab fa-github"></i>
-          <span>Source and other projects.</span>
-        </a>
-      </div>
-    </div>
-    <div className="footer-tri" />
-  </footer>
+  <StaticQuery
+    query={query}
+    render={data => {
+      const { siteMetadata } = data.site;
+      return (
+        <footer>
+          <div className="footer-wrapper">
+            <div className="footer-info">
+              <h5 dangerouslySetInnerHTML={{ __html: siteMetadata.title }}/>
+              <span style={{ fontSize: '0.85rem' }}>© {new Date().getFullYear()}, By Ori Perelman</span>
+            </div>
+            <div className="contact">
+              <a href="mailto: oriperelman@gmail.com">
+                <i className="far fa-envelope"></i>
+                <span>Drop a line.</span>
+              </a>
+              <a href={siteMetadata.author} target="_blank" rel="noopener noreferrer">
+                <i className="fab fa-github"></i>
+                <span>Source and other projects.</span>
+              </a>
+            </div>
+          </div>
+          <div className="footer-tri" />
+        </footer>
+      )
+    }}
+  />
 )
 
 export default Footer;
