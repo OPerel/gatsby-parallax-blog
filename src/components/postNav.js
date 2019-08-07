278bb80a61a141e1f01339c4cd297256312574ae
Operel <oriperelman@gmail.com>
2019-08-07 11:28:13 +0300

    install styled components, some configs and change postNav to styles components

--- src/components/postNav.js
+++ src/components/postNav.js
@@ -1,26 +1,48 @@
 import React from "react";
 import { Link } from 'gatsby';
+import styled from 'styled-components';
 import "../assets/allStyles.css"
 
+const PostNavDiv = styled.div`
+  display: flex;
+  justify-content: space-around;
+  max-width: 50%;
+  height: 10vh;
+  margin: 8% auto;
+`
+
+const ArrowLink = styled.i`
+  display: block !important;
+  margin: 0 0 3% 0;
+  font-size: 1.6rem;
+  text-align: center;
+`
+
+const StyledLink = styled(Link)`
+  :hover {
+    text-decoration: none;
+  }
+`
+
 const PostNav = ({ prev, next }) => (
-  <div className="post-nav">
+  <PostNavDiv>
     {next && (
-      <div className="arrow-link">
-        <Link to={next.fields.slug}>
-          <i className="far fa-arrow-alt-circle-left"></i>
+      <div>
+        <StyledLink to={next.fields.slug}>
+          <ArrowLink className="far fa-arrow-alt-circle-left"></ArrowLink>
           <span>Previous</span>
-        </Link>
+        </StyledLink>
       </div>
     )}
     {prev && (
-      <div className="arrow-link">
-        <Link to={prev.fields.slug}>
-          <i className="far fa-arrow-alt-circle-right"></i>
+      <div>
+        <StyledLink to={prev.fields.slug}>
+          <ArrowLink className="far fa-arrow-alt-circle-right"></ArrowLink>
           <span>Next</span>
-        </Link>
+        </StyledLink>
       </div>
     )}
-  </div>
+  </PostNavDiv>
 )
 
 
